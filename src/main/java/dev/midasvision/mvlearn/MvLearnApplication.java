4404a7ee9b4ff97e533eda8cad7b187f41990d02
avnercaleb <avnercaleb@hotmail.com>
2024-02-28 21:04:11 -0300

    Criação do Projeto

---
+++ src/main/java/dev/midasvision/mvlearn/MvLearnApplication.java
@@ -0,0 +1,13 @@
+package dev.midasvision.mvlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/midasvision/mvlearn/MvLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.midasvision.mvlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
